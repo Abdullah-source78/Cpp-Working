e41682c6576a5704459751012f4002a14d308fc9
Peer Abdulllah <chaudharyabdullah254@gmail.com>
2024-12-11 17:41:00 +0500

    New Addition

---
+++ Q3_EvenOdd.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void display_even_odd(int arr[],int SIZE){
+    for(int i = 0;i<SIZE;i++){
+        cout<<"Enter value at "<<i<<" index : ";
+        cin>>arr[i];
+
+    }
+    for(int i = 0;i<SIZE;i++){
+        if(arr[i]%2==0){
+            cout<<"Value at "<<i<<" index is : "<<arr[i] << " is Even :"<<endl;
+        }else{
+            cout<<"Value at "<<i<<" index is : "<<arr[i] << " is odd :"<<endl;
+
+        }
+    }
+}
+int main(){
+
+    int const SIZE = 5;
+    int arr[SIZE] ;
+    display_even_odd(arr,SIZE);
+
+
+
+}
